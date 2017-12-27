59a4cf4fe465bbaa82b838a18331e3c108cc0a0b
Kyriet <kyriet.sebastian@gmail.com>
2017-12-27 21:25:49 +0100

    Naprawiono potencjalne wyjście poza zakres

--- src/InputParser.cpp
+++ src/InputParser.cpp
@@ -36,16 +36,18 @@ vector< string > InputParser::split( string & cmd )
         // to wczytuj do napotkania znaku ("). Znaki (") s¹ wyrzucane.
         if( cmd[ i ] == '\"' && parameters.size() > 0 )
         {
-            for( i = i + 1; cmd[ i ] != '\"'; i++ )
+			i = i + 1;
+			do
             {
-                // B³¹d parsowania. Zwraca vektor "help"
+                // B³¹d parsowania. Zwraca pusty wektor.
                 if( i >= cmd.size() )
                 {
                     vector< string > vec;
                     return vec;
                 }
                 element += cmd[ i ];
-            }
+				i++;
+			} while( cmd[ i ] != '\"' );
             parameters.push_back( element );
             element = "";
         }
